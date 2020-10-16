a7eb1f79bad7cefe5b0252859d6f6b8980724e6c
luketatro <luketatro@pop-os.localdomain>
2020-10-16 00:43:57 -0400

    user_provisioning.sh created

---
+++ user_provisiong.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+usage()
+{
+ echo "program is running"  
+}
+
+checkRoot()
+{
+  userid=$(id -u)
+  echo "The current user is: $(whoami)"
+
+  if [ "$userid" -eq 0 ]; then
+    echo "You are logged in as root" 
+  else 
+    echo "$(whoami) is not root"
+    echo "You must be root to execute this program" 
+    exit 1 
+  fi
+
+}
+
+
+
+
+#MAIN
+
+PROGNAME="$basename"$0")" 
+
+checkRoot
+
+if [ -e "$1" ]; then 
+ for user in $(cat "$1"| cut -d"@" -f 1); 
+  do echo  $user;  
+     $(useradd $user -m -p $(openssl passwd -1 $password) -s /bin/bash); 
+ done
+else 
+  echo "Did not work" 
+fi 
+
+
+#for user in $(cat /home/luketatro/bin/emails.txt | cut -d"@" -f 1); do echo  $user; done
+
+echo " " 
+echo " " 
+usage
+
